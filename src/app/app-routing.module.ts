9fbea267ff4ccfa22f1863b6672c49a126fc3da0
Prashant sonar <sonarprashant151@gmail.com>
2019-02-04 12:38:01 +0530

    new branch for Book Store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllbooksComponent } from "./books/allbooks/allbooks.component";
+import { SpecbookComponent } from "./books/specbook/specbook.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'allbooks', component: AllbooksComponent },
+  { path: 'Inspbook', component: SpecbookComponent , data:{ bookType : 'Inspirational'}},
+  { path: 'Ficbook', component: SpecbookComponent , data:{ bookType : 'Fictional'}},
+  { path: 'Selfbook', component: SpecbookComponent , data:{ bookType : 'Selfhelp'}},
+  { path: '**', redirectTo: 'allbooks' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { SidebarComponent } from './ui/sidebar/sidebar.component';
+import { AllbooksComponent } from './books/allbooks/allbooks.component';
+import { SpecbookComponent } from './books/specbook/specbook.component';
+import { BooksService } from './shared/service/books.service';
+import { AddBookComponent } from './books/add-book/add-book.component';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    AllbooksComponent,
+    SpecbookComponent,
+    AddBookComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  entryComponents: [
+    AddBookComponent
+  ],
+  providers: [ BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
